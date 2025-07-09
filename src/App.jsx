98b4b5225989b4098cafa1756aa88edd1f967e4a
Pablo Valverde <pblvalverde@gmail.com>
2025-07-08 23:15:44 -0500

    fix: switch to HashRouter to resolve GitHub Pages 404 issues
    - Change from BrowserRouter to HashRouter in App.jsx
    - Simplify 404.html to redirect to home page
    - Remove unnecessary SPA redirect scripts from index.html
    - HashRouter uses # URLs which work perfectly with GitHub Pages
    - URLs will now be /#/path instead of /path
    - This completely eliminates 404 errors on page refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme, CssBaseline, Box } from '@mui/material';
 import NavBar from './NavBar';
 import HomePage from './LandingPage';
